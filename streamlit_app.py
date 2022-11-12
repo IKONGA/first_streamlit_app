24f85fc852c47ead0fee25695a2d5bb3298c04cd
NINON IKONGA <117132123+IKONGA@users.noreply.github.com>
2022-11-12 18:54:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,5 +56,5 @@ streamlit.dataframe(my_data_rows)
 
 # allow end user
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks to adding jackfruit ', add_my_fruit)
+streamlit.write('Thanks to adding jackfruit ')
 
