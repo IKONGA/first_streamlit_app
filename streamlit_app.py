6e1c745403187ca55545a9f33ef803aa19bba026
NINON IKONGA <117132123+IKONGA@users.noreply.github.com>
2022-11-13 23:06:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 #fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Cantaloupe'])
+#fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Cantaloupe'])
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Banana'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
@@ -33,7 +34,7 @@ streamlit.dataframe(fruits_to_show)
 # new section
 streamlit.header("Fruityvice Fruit Advice!")
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Orange')
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Orange')
 streamlit.write('The user entered ', fruit_choice)
 #import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
