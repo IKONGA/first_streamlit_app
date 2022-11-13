955568d3b7837c2dd96bbae39579be1cd8c3f7c5
NINON IKONGA <117132123+IKONGA@users.noreply.github.com>
2022-11-14 00:43:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,28 +70,39 @@ except URLError as e:
   streamlit.error()
 
 # don't run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
+#my_cur.execute("SELECT * from fruit_load_list")
 # my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
+#my_data_rows = my_cur.fetchall()
 # streamlit.text("Hello from Snowflake:")
 # streamlit.text("The fruits load list contains")
 streamlit.header("The fruits load list contains")
 # streamlit.text(my_data_row)
 # streamlit.dataframe(my_data_row)
-streamlit.dataframe(my_data_rows)
+#streamlit.dataframe(my_data_rows)
 
-# # # 
+
+# Snowflake-reloated functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("SELECT * from fruit_load_list")
+         return my_cur.fetchall()
+#Add a button to load the fruit
+if streamlit.button('Get fruit load list'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows=get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
+
 # allow end user
 # add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Canteloupe')
-streamlit.write('Thanks to adding  ', add_my_fruit)
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?','Canteloupe')
+#streamlit.write('Thanks to adding  ', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlist')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlist')")
 
 
