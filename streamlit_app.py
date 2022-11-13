fcc828d18e0d1e1eec66e2890e9c9392de0e6960
NINON IKONGA <117132123+IKONGA@users.noreply.github.com>
2022-11-14 00:06:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,13 +47,13 @@ streamlit.header("Fruityvice Fruit Advice!")
 #---streamlit.dataframe(fruityvice_normalized)
 # create the  repeatable code block(called function)
 def get_fruitivice_data(this_fruit_choise):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choise)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-      streamlit.error("Please select a fruit to get information.")
+#  if not fruit_choice:
+#      streamlit.error("Please select a fruit to get information.")
   else:
 #      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 #      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
